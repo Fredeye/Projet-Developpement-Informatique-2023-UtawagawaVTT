b3002b2cb60bda9a80c823b4442cb6142480bf74
Fredeye <122447185+Fredeye@users.noreply.github.com>
2023-05-16 15:42:14 +0200

    Add files via upload

--- zone_etude/script/script_zone_etude.sh
+++ zone_etude/script/script_zone_etude.sh
@@ -8,20 +8,22 @@ wget http://files.opendatarchives.fr/professionnels.ign.fr/rgealti/RGEALTI_2-0_5
 wget --no-check-certificate https://tinitaly.pi.ingv.it/data_1.1/w50030_s10/w50030_s10.zip -P ../telechargement_mnt/italie
 
 # Extraction des données
-mkdir -p ../telechargement_mnt/RGEALTI_MNT_5M_ASC_LAMB93_IGN69_D073/asc && 7z x ../telechargement_mnt/RGEALTI_MNT_5M_ASC_LAMB93_IGN69_D073/*.7z -o../telechargement_mnt/RGEALTI_MNT_5M_ASC_LAMB93_IGN69_D073/asc -aos
-mkdir -p ../telechargement_mnt/italie/asc && 7z x ../telechargement_mnt/italie/*.zip -o../telechargement_mnt/italie/asc -aos
+mkdir -p ../telechargement_mnt/RGEALTI_MNT_5M_ASC_LAMB93_IGN69_D073/asc && \
+ls ../telechargement_mnt/RGEALTI_MNT_5M_ASC_LAMB93_IGN69_D073/*.7z | xargs -n 1 7z x -o../telechargement_mnt/RGEALTI_MNT_5M_ASC_LAMB93_IGN69_D073/asc -aos
 
+mkdir -p ../telechargement_mnt/italie/asc && \
+ls ../telechargement_mnt/italie/*.zip | xargs -n 1 7z x -o../telechargement_mnt/italie/asc -aos
 
 # Production MBTILES zoom 0 à 11
 rm -fr ../z_0_16 && mkdir -p ../z_0_16
 
 # Reprojection des tuiles ASC en EPSG:3857
-for file in ../telechargement_mnt/RGEALTI_MNT_5M_ASC_LAMB93_IGN69_D073/asc/*.asc; do
-    gdalwarp -s_srs EPSG:2154 -t_srs EPSG:3857 $file ../z_0_16/$(basename ${file%.*}).tif
+find ../telechargement_mnt/RGEALTI_MNT_5M_ASC_LAMB93_IGN69_D073/asc -type f -name "*.asc" | while read -r file; do
+    gdalwarp -s_srs EPSG:2154 -t_srs EPSG:3857 "$file" "../z_0_16/$(basename "${file%.*}").tif"
 done
 
-for file in ../telechargement_mnt/italie/asc/*.tif; do
-    gdalwarp -t_srs EPSG:3857 $file ../z_0_16/$(basename ${file%.*}).tif
+find ../telechargement_mnt/italie/asc -type f -name "*.tif" | while read -r file; do
+    gdalwarp -t_srs EPSG:3857 "$file" "../z_0_16/$(basename "${file%.*}").tif"
 done
 
 # Assemblage des tuiles ASC en une image virtuelle
